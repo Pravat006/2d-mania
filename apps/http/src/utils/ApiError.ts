ba3a912d8dbc937a5809fddfec70b033efa46e89
PRAVAT BEHERA <beherapravat301@gmail.com>
2025-03-21 22:41:26 +0530

    feat(utils): add ApiError and ApiResponse classes for standardized API responses, and i error handling, asyncHandler for controller initialization

---
+++ apps/http/src/utils/ApiError.ts
@@ -0,0 +1,31 @@
+class ApiError<T> extends Error {
+    statusCode: number;
+    message: string;
+    errors: any[];
+    stack?: string;
+    data: T;
+    success: boolean;
+  
+    constructor(
+      statusCode: number,
+      message: string = "Something went wrong",
+      errors: any[] = [],
+      stack: string = "",
+      data: T
+    ) {
+      super(message);
+      this.statusCode = statusCode;
+      this.data = data;
+      this.message = message;
+      this.success = false;
+      this.errors = errors;
+  
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+      }
+    }
+  }
+  
+  export { ApiError };
